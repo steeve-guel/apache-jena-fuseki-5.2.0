b3472a71233059142189b9fe2ab80a18c7e3f338
Guel Steeve <steevezguel@gmail.com>
2025-03-29 16:16:27 +0000

    #Udpate emplace start.sh

--- run/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x fuseki-server
-./fuseki-server --config=run/config.ttl
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Override Ruby command
+./apache-jena-fuseki-4.10.0/fuseki-server --config=run/config.ttl --port=$PORT
